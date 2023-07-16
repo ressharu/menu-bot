f3cc3f9fe1247e41824874a8f48fad22555e2053
tshi360 <ebideit360@gmail.com>
2023-07-16 10:35:07 +0900

    アクセス制限を追加

--- src/db/router.js
+++ src/db/router.js
@@ -3,10 +3,23 @@ import bodyParser from "body-parser";
 
 import push from "./datapush.js";
 import pull from "./datapull.js";
+import { nextTick } from "process";
 
 const router = express.Router();
 
-router.post("/api", bodyParser.json(), async (req, res) => {
+router.post("/api", bodyParser.json(), (req, res, next) => {
+	const host = req.headers.host;
+	if (host == null) {
+		res.send(400);
+		return;
+	}
+	
+	if (host === process.env.SCRAPING_URL) {
+		next();
+	} else {
+		res.send(400);
+	}
+}, async (req, res) => {
 	console.log(req.body);
 	push(req.body);
 	res.end();
